5dca6be6e69b9e5b00682c4449aeacb7847ebc78
mpolev <mpolev.cs@gmail.com>
2014-04-30 07:07:51 +0400

    Revert "Push project"
    This reverts commit 5e9526ac31c43e11f110ec12078df9ecb4844ef0.

--- ApiVK/src/main/java/com/example/apivk/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.example.apivk;
-
-public class Constants {
-    //Этот ID предназначен только для примера. Пожалуйста замение его ID своего приложения.
-    public static String API_ID="4098407";
-
-}
